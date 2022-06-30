c76809c48464423820036a9a2c6dcc85f04848f3
islomjon <turgunovislomjon107@gmail.com>
2022-06-30 17:21:14 +0300

    add registration/login/logout pages

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("jblog.urls")),
+    path('registration/', include('django.contrib.auth.urls')),
+    path('registration/', include('regsitration.urls')),
 ]
--- jblog/urls.py
+++ jblog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('update_post/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('delete_post/<int:pk>/remove', PostDeleteView.as_view(), name='delete_post'),
+    
    
 ]
