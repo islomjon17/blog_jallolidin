8f5360f2edb6559cb5255110518c1657a9490152
islomjon <turgunovislomjon107@gmail.com>
2022-07-02 13:00:00 +0300

    add region choices

--- jblog/admin.py
+++ jblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, Category
 
 
 # Register your models here.
@@ -7,3 +7,4 @@ from .models import BlogPost
 
 
 admin.site.register(BlogPost)
+admin.site.register(Category)
--- jblog/models.py
+++ jblog/models.py
@@ -9,12 +9,21 @@ class Category(models.Model):
     def __str__(self):
         return (self.name)
 
-   
+    def get_absolute_url(self):
+        return reverse('home' )  
 
 
 STATUS = (
     (0, "Draft"),
     (1, "Published")
+)
+
+REGION = (
+    ("Andijon", "Andijon"),("Samarqand", "Samarqand"),("Sirdaryo", "Sirdaryo"),
+    ("Buxoro", "Buxoro"),("Qashqadaryo", "Qashqadaryo"),("Surxondaryo", "Surxondaryo"),
+    ("Farg'ona", "Farg'ona"),("Navoiy", "Navoiy"),("Toshkent", "Toshkent"),
+    ("Jizzax", "Jizzax"),("Namangan", "Namangan"),("Qoraqalpag'iston", "Qoraqalpag'iston"),
+    ("Xorazm", "Xorazm"),
 )
 
 
@@ -30,6 +39,7 @@ class BlogPost(models.Model):
     created_on = models.DateTimeField(auto_now_add=True) #Date of creation
     updated_on = models.DateTimeField(auto_now=True) #Date of updation
     status = models.IntegerField(choices=STATUS, default=0) #Status of the Article either Draft or Published
+    region = models.CharField(max_length=250, choices=REGION, default="Toshkent") #Status of the Article regions
 
     def __str__(self):
         return f"({self.title}) ({self.author})"
