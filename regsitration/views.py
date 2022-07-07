9fc8217f7c000177a77d912516a5b5c85a88e378
islomjon <turgunovislomjon107@gmail.com>
2022-07-07 07:52:09 +0300

    finish edit_profile page

--- regsitration/views.py
+++ regsitration/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from .forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 # Sign Up View
+
+
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+
+class UserEditView(UpdateView):
+    form_class = UserChangeForm
+
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
