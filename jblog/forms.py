9c4f049fd0f0367364755f2cc0a8c8df080b95f3
islomjon <turgunovislomjon107@gmail.com>
2022-07-07 07:37:19 +0300

    add snippet field

--- jblog/forms.py
+++ jblog/forms.py
@@ -8,12 +8,13 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ('title', 'slug', 'author', 'description',
-                  'category', 'keywords', 'cover', 'status')
+                  'category', 'keywords', 'cover', 'status', 'snippet')
 
         widgets = {
 
             'title':  forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'title'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'slug'}), 'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'author'}), 'region': forms.Select(attrs={'class': 'form-control', 'placeholder': 'region'}), 'status': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'status'}), 'keywords': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'keywords'}), 'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'author'}), 'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'description'})
+            'slug': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'slug'}), 'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'author'}), 'region': forms.Select(attrs={'class': 'form-control', 'placeholder': 'region'}), 'status': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'status'}), 'keywords': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'keywords'}), 'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'author'}), 'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'description'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'description'})
 
         }
 
